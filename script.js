10f879bfa022c8c6ecc1c013fda1b5d762bf0517
Alexia Kattah <alexiakattah@gmail.com>
2023-09-25 11:04:07 -0300

    feature: criando um gerador de cpf

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log('Hello World');
+//452.458.458-45
+function generateCPF() {
+  const number1 = generateRandomNumber();
+  const number2 = generateRandomNumber();
+  const number3 = generateRandomNumber();
+  const checkDigit1 = calculateCheckDigit(number1, number2, number3);
+  const checkDigit2 = calculateCheckDigit(
+    number1,
+    number2,
+    number3,
+    checkDigit1,
+  );
+  let cpf;
+  if (document.getElementById('dotsAndDashesTrue').checked) {
+    cpf = `${number1}.${number2}.${number3}-${checkDigit1}${checkDigit2}`;
+  } else {
+    cpf = `${number1}${number2}${number3}${checkDigit1}${checkDigit2}`;
+  }
+
+  document.getElementById('inputResult').value = cpf;
+}
+
+function generateRandomNumber() {
+  const random = Math.floor(Math.random() * 999);
+  const formatted = random.toString().padStart(3, '0');
+  return formatted;
+}
+
+function calculateCheckDigit(number1, number2, number3, number4) {
+  const numbers = `${number1}${number2}${number3}`.split('');
+  if (number4 !== undefined) {
+    numbers.push(number4);
+  }
+
+  let sum = 0;
+  let index = 0;
+  const initialNumber = number4 !== undefined ? 11 : 10;
+
+  for (let i = initialNumber; i >= 2; i--) {
+    sum += parseInt(numbers[index]) * i;
+    index++;
+  }
+  const rest = sum % 11;
+  if (rest < 2) {
+    return 0;
+  } else {
+    return 11 - rest;
+  }
+}
